6a23803c451067511984a32a9195aab4cadfb3ca
Bobby Aleem <bobbybeatmaker@hotmail.com>
2022-01-25 20:00:02 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php include_once("comet-loader/index.html"); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME</title>
+</head>
+<body>
+    <div>
+        <a href="./comet-loader/index.html">Comet Loader</a>
+    </div>
+</body>
+</html>
